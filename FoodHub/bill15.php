3e27de06ef3b4822b72504de98b02aa591762632
Muhammed Nafi K P <142255723+nafinaas@users.noreply.github.com>
2023-12-13 17:41:37 +0530

    Add files via upload

---
+++ FoodHub/bill15.php
@@ -0,0 +1,31 @@
+<?php
+$product="Mutton Biriyan";
+$price="480";
+$md1 = 100;
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "bills";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+else{
+    $stmt = $conn->prepare("insert into details1(product,price)values(?,?)");
+    $stmt->bind_param("ss",$product,$price);
+    $execval = $stmt->execute();
+    echo $execval;
+}
+$sql = "UPDATE details1 SET md1=md1-1 WHERE md1>0";
+
+if ($conn->query($sql) === TRUE) {
+  header("location:confirm.html");
+} else {
+  echo "Error updating record: " . $conn->error;
+}
+
+$conn->close();
+?>
